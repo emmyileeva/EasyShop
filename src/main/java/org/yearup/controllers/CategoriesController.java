da5d0c6ec73875241d30f17aadad5043a94fdd3d
Emiliya Ileeva <emilyileeva@gmail.com>
2025-06-23 13:39:48 -0700

    Implement PUT /categories/{id} endpoint (ADMIN only)

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -68,9 +68,12 @@ public class CategoriesController
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id
+        categoryDao.update(id, category);
     }
 
 
