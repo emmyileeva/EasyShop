f44f76212b8e4405853d304a600f5f13a3b351b1
Emiliya Ileeva <emilyileeva@gmail.com>
2025-06-24 16:13:38 -0700

    Add OrderDao and OrderLineItemDao with MySQL implementations

---
+++ src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -0,0 +1,48 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderDao;
+import org.yearup.models.Order;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDate;
+
+@Component
+public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
+
+    public MySqlOrderDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public Order create(Order order) {
+        String sql = """
+            INSERT INTO orders (user_id, date, address, city, state, zip, shipping_amount)
+            VALUES (?, ?, ?, ?, ?, ?, ?)
+        """;
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
+            stmt.setInt(1, order.getUserId());
+            stmt.setDate(2, Date.valueOf(LocalDate.now()));
+            stmt.setString(3, order.getAddress());
+            stmt.setString(4, order.getCity());
+            stmt.setString(5, order.getState());
+            stmt.setString(6, order.getZip());
+            stmt.setBigDecimal(7, order.getShippingAmount());
+
+            stmt.executeUpdate();
+
+            ResultSet keys = stmt.getGeneratedKeys();
+            if (keys.next()) {
+                order.setOrderId(keys.getInt(1));
+            }
+
+            return order;
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to insert order.", e);
+        }
+    }
+}
---
+++ src/main/java/org/yearup/data/mysql/MySqlOrderLineItemDao.java
@@ -0,0 +1,41 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderLineItemDao;
+import org.yearup.models.OrderLineItem;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+@Component
+public class MySqlOrderLineItemDao extends MySqlDaoBase implements OrderLineItemDao {
+
+    public MySqlOrderLineItemDao(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public void create(OrderLineItem item) {
+        String sql = """
+            INSERT INTO order_line_items (order_id, product_id, sales_price, quantity, discount)
+            VALUES (?, ?, ?, ?, ?)
+        """;
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, item.getOrderId());
+            stmt.setInt(2, item.getProductId());
+            stmt.setBigDecimal(3, item.getSalesPrice());
+            stmt.setInt(4, item.getQuantity());
+            stmt.setBigDecimal(5, item.getDiscount());
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to insert order line item.", e);
+        }
+    }
+}
+
